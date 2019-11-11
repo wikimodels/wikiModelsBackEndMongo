4ce23ec12ce53b9e1c27acf341ee0771dcd6edce
wikimodelsstore <vitali.wiki.models@gmail.com>
2019-11-11 21:21:17 +0300

    updated 'get article by id' function

--- functions/f_01_get_article_by_id/source.js
+++ functions/f_01_get_article_by_id/source.js
@@ -1,15 +1,15 @@
-exports = async function(articleId) {
+exports = async function(article_id) {
 
     try {
 
         const coll = await context.functions.execute("util_db_get_articles_collection");
 
-        return await coll.findOne({ _id: BSON.ObjectId(articleId) });
+        return await coll.findOne({ article_id: article_id });
 
 
     } catch (e) {
 
-        await context.functions.execute("util_db_log_error", "f_3_get_total_clicks_for_time_period", e);
+        await context.functions.execute("util_db_log_error", "f_01_get_aricle_by_id", e);
         return Error(e.message);
     }
 }
